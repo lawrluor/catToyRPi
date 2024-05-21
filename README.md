86694bf06f820c68d1a2982a7f71cb5c866a2e64
Lawrence Luo <luolawrence1@gmail.com>
2024-05-20 21:01:07 -0700

    added quickstart file

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
+NOTE: This was tested with Raspberry Pi v1 B+, released in 2014.
 
 # Set Up Hardware
 https://projects.raspberrypi.org/en/projects/raspberry-pi-setting-up/1
-Connect mouse, keyboard, HDMI display, ethernet (if Raspberry Pi 2 or below)
+
+Install OS onto the microSD card.
+
+Then, to the Raspberry Pi, connect microSD card, mouse, keyboard, HDMI display, ethernet (if Raspberry Pi 2 or below)
 
 # Set Up Software
 
@@ -23,6 +27,14 @@ In this version:
 # RealVNC (for Virtual Access to Raspberry Pi)
 Connect via RealVNC: https://help.realvnc.com/hc/en-us/articles/360002249917-RealVNC-Connect-and-Raspberry-Pi#getting-connected-to-your-raspberry-pi-0-1
 
+```sh
+sudo apt-get install realvnc-vnc-viewer
+```
+
+Then, go to the Raspberry Pi icon in the top left toolbar -> Internet -> VNC Server.
+Enter your credentials for the Raspberry Pi if prompted
+After the VNC Server opens, on your other viewing computer, either enter the IP address of the Raspberry Pi listed, or if it is a personal device, log in to VNC Server using RealVNC account credentials to have the viewing computer automatically store and remember this computer.
+
 (at this point, should be able to disconnect HDMI/external monitor and access the desktop of the Raspberry Pi virtually)
 
 # Install git (for personal devices only)
@@ -42,5 +54,20 @@ pip install -r requirements.txt
 python app.py
 ```
 
-# For private/proprietary files: SCP instead
+# For private/proprietary files:
+Use scp or RealVNC file transfer functionality instead.
 
+To transfer files to your Raspberry Pi, click the cycling arrows button and follow the instructions.
+
+To transfer files from your Raspberry Pi, use RealVNC Viewer to open the RealVNC Server dialog remotely, select Menu > File transfer, and follow the instructions. Detailed steps are here.
+
+
+# QUICKSTART (Personal Device Setup)
+Use RealVNC file transfer to transfer the `quickstart.sh` file (available in the repo), or run:
+
+```sh
+git clone https://github.com/lawrluor/catToyRPi.git
+bash catToyRpi/quickstart.sh
+```
+
+This will automatically create a `Repos` directory and `cd` into it.
---
+++ quickstart.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Script to set up Raspberry Pi for a cat toy project
+
+echo "Starting Raspberry Pi setup..."
+
+# Update the system
+echo "Updating system..."
+sudo apt update && sudo apt upgrade -y
+
+# Install RealVNC
+echo "Installing RealVNC..."
+sudo apt-get install -y realvnc-vnc-viewer
+
+# Configure RealVNC
+echo "Configuring RealVNC..."
+# RealVNC should be configured manually as per instructions on the Raspberry Pi documentation
+echo "Please go to the Raspberry Pi icon in the top left toolbar -> Internet -> VNC Server."
+echo "Enter your credentials for the Raspberry Pi if prompted."
+echo "After the VNC Server opens, on your other viewing computer, either enter the IP address of the Raspberry Pi listed,"
+echo "or if it is a personal device, log in to VNC Server using RealVNC account credentials to have the viewing computer automatically store and remember this computer."
+
+# Install Git
+echo "Installing Git..."
+sudo apt install -y git
+git config --global credential.helper store
+
+# Clone the repository and set up the environment
+mkdir Repos
+cd Repos
+echo "Cloning repository and setting up the environment..."
+git clone https://github.com/lawrluor/catToyRPi.git
+cd catToyRPi || { echo "Failed to enter the repository directory"; exit 1; }
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+echo "Setup completed. You can now use your Raspberry Pi as a cat toy controller."
+
+# Instructions for servo motor connection
+echo "To connect the servo motor:"
+echo "- Red wire: 1st row, 2nd column"
+echo "- Black wire (ground): 5th row, 1st column"
+echo "- Orange wire: 6th row, 1st column"
+
+# Instructions for file transfer
+echo "To transfer private/proprietary files, use scp or RealVNC file transfer functionality."
+echo "For RealVNC file transfer, click the cycling arrows button and follow the instructions."
+echo "To transfer files from your Raspberry Pi, use RealVNC Viewer to open the RealVNC Server dialog remotely, select Menu > File transfer, and follow the instructions."
+echo "Detailed steps are available on the RealVNC documentation."
+
+echo "Setup script finished."
+
+# Run the application
+echo "Starting the application..."
+echo "Use python app.py to start the server in the future."
+python app.py
